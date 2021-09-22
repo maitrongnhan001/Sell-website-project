63889f4c98c1e4b24767078eb2cebf7cebdffe3e
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-09-22 21:51:53 +0700

    added layout for admin manager

---
+++ Admin/manager-admin.php
@@ -0,0 +1,83 @@
+<?php include('./layouts/header.php') ?>
+
+<section class="main text-content">
+    <div class="container">
+        <h1>Quản lý nhân viên</h1>
+        <br>
+        <a href="#" class="btn-200 btn-primary">Thêm nhân viên</a>
+        <br><br>
+        <table class="tbl-manager">
+            <tr>
+                <th>STT</th>
+                <th>Họ và Tên</th>
+                <th>Chức vụ</th>
+                <th>Địa chỉ</th>
+                <th>Số điện thoại</th>
+                <th>Quản lý</th>
+            </tr>
+            <tr class="white text-center">
+                <td>1</td>
+                <td>Mai Trọng Nhân</td>
+                <td>Quản lý</td>
+                <td>Cà Mau</td>
+                <td>0943363414</td>
+                <td>
+                    <a href="#" class="btn-primary">Cập nhật</a>
+                    <a href="#" class="btn-danger">Xoá</a>
+                    <div class="clear-fix"></div>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <td>2</td>
+                <td>Mai Trọng Nhân</td>
+                <td>Quản lý</td>
+                <td>Cà Mau</td>
+                <td>0943363414</td>
+                <td>
+                    <a href="#" class="btn-primary">Cập nhật</a>
+                    <a href="#" class="btn-danger">Xoá</a>
+                    <div class="clear-fix"></div>
+                </td>
+            </tr>
+            <tr class="white text-center">
+                <td>3</td>
+                <td>Mai Trọng Nhân</td>
+                <td>Quản lý</td>
+                <td>Cà Mau</td>
+                <td>0943363414</td>
+                <td>
+                    <a href="#" class="btn-primary">Cập nhật</a>
+                    <a href="#" class="btn-danger">Xoá</a>
+                    <div class="clear-fix"></div>
+                </td>
+            </tr>
+            <tr class="text-center">
+                <td>4</td>
+                <td>Mai Trọng Nhân</td>
+                <td>Quản lý</td>
+                <td>Cà Mau</td>
+                <td>0943363414</td>
+                <td>
+                    <a href="#" class="btn-primary">Cập nhật</a>
+                    <a href="#" class="btn-danger">Xoá</a>
+                    <div class="clear-fix"></div>
+                </td>
+            </tr>
+            <tr class="white text-center">
+                <td>5</td>
+                <td>Mai Trọng Nhân</td>
+                <td>Quản lý</td>
+                <td>Cà Mau</td>
+                <td>0943363414</td>
+                <td>
+                    <a href="#" class="btn-primary">Cập nhật</a>
+                    <a href="#" class="btn-danger">Xoá</a>
+                    <div class="clear-fix"></div>
+                </td>
+            </tr>
+        </table>
+        
+    </div>
+</section>
+
+<?php include('./layouts/footer.php') ?>
