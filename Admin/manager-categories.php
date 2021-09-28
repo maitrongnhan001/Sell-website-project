fd331ad6e0e6f925c615aff2df7a3a3935df4d24
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-09-28 18:56:50 +0700

    added function display list categories

--- Admin/manager-categories.php
+++ Admin/manager-categories.php
@@ -7,9 +7,9 @@
         <a href=<?php echo URL . "admin/add-category.php"; ?> class="btn-200 btn-primary">Thêm danh mục</a>
         <?php
         //show nofication add admin successfully
-        if (isset($_SESSION['status_user'])) {
-            echo ("<br><div class='green'>" . $_SESSION['status_user'] . "</div>");
-            unset($_SESSION['status_user']);
+        if (isset($_SESSION['status_category'])) {
+            echo ("<br><div class='green'>" . $_SESSION['status_category'] . "</div>");
+            unset($_SESSION['status_category']);
         }
         if (isset($_SESSION['error'])) {
             echo ("<br><div class='red'>" . $_SESSION['error'] . "</div>");
@@ -24,56 +24,44 @@
                 <th>Hình ảnh</th>
                 <th>Quản lý</th>
             </tr>
-            <tr class="white text-center">
-                <td>1</td>
-                <td>Bánh gì đó</td>
-                <td><a href="../images//categories/Food_Category_530.jpg"><img src="../images//categories/Food_Category_530.jpg" width="100px" height="100px" alt="No image" class="img-category"></a></td>
-                <td>
-                    <a href=<?php echo URL . "admin/update-admin.php?id=" . $id; ?> class="btn-primary">Cập nhật</a>
-                    <a href=<?php echo URL . "admin/delete-item.php?id=" . $id . "&type=1"; ?> class="btn-danger">Xoá</a>
-                    <div class="clear-fix"></div>
-                </td>
-            </tr>
-            <tr class="text-center">
-                <td>1</td>
-                <td>Bánh gì đó</td>
-                <td><a href="../images//categories/Food_Category_530.jpg"><img src="../images//categories/Food_Category_530.jpg" width="100px" height="100px" alt="No image" class="img-category"></a></td>
-                <td>
-                    <a href=<?php echo URL . "admin/update-admin.php?id=" . $id; ?> class="btn-primary">Cập nhật</a>
-                    <a href=<?php echo URL . "admin/delete-item.php?id=" . $id . "&type=1"; ?> class="btn-danger">Xoá</a>
-                    <div class="clear-fix"></div>
-                </td>
-            </tr>
-            <tr class="white text-center">
-                <td>1</td>
-                <td>Bánh gì đó</td>
-                <td><a href="../images//categories/Food_Category_530.jpg"><img src="../images//categories/Food_Category_530.jpg" width="100px" height="100px" alt="No image" class="img-category"></a></td>
-                <td>
-                    <a href=<?php echo URL . "admin/update-admin.php?id=" . $id; ?> class="btn-primary">Cập nhật</a>
-                    <a href=<?php echo URL . "admin/delete-item.php?id=" . $id . "&type=1"; ?> class="btn-danger">Xoá</a>
-                    <div class="clear-fix"></div>
-                </td>
-            </tr>
-            <tr class="text-center">
-                <td>1</td>
-                <td>Bánh gì đó</td>
-                <td><a href="../images//categories/Food_Category_530.jpg"><img src="../images//categories/Food_Category_530.jpg" width="100px" height="100px" alt="No image" class="img-category"></a></td>
-                <td>
-                    <a href=<?php echo URL . "admin/update-admin.php?id=" . $id; ?> class="btn-primary">Cập nhật</a>
-                    <a href=<?php echo URL . "admin/delete-item.php?id=" . $id . "&type=1"; ?> class="btn-danger">Xoá</a>
-                    <div class="clear-fix"></div>
-                </td>
-            </tr>
-            <tr class="white text-center">
-                <td>1</td>
-                <td>Bánh gì đó</td>
-                <td><a href="../images//categories/Food_Category_530.jpg"><img src="../images//categories/Food_Category_530.jpg" width="100px" height="100px" alt="No image" class="img-category"></a></td>
-                <td>
-                    <a href=<?php echo URL . "admin/update-admin.php?id=" . $id; ?> class="btn-primary">Cập nhật</a>
-                    <a href=<?php echo URL . "admin/delete-item.php?id=" . $id . "&type=1"; ?> class="btn-danger">Xoá</a>
-                    <div class="clear-fix"></div>
-                </td>
-            </tr>
+            <?php
+            //get data of category
+            $conn = connectToDatabase();
+            $sql = "SELECT * FROM LoaiHangHoa";
+            $listCategories = executeSQLResult($conn, $sql);
+            for ($i = 1; $i <= count($listCategories); $i++) {
+                $id = $listCategories[$i - 1]['MaLoaiHang'];
+                $NameCategory = $listCategories[$i - 1]['TenLoaiHang'];
+                $pathImage = URL . 'images/categories/' . $listCategories[$i - 1]['HinhAnh'];
+                if ($i % 2 == 0) {
+            ?>
+                    <tr class="text-center">
+                        <td><?php echo $i; ?></td>
+                        <td><?php echo $NameCategory; ?></td>
+                        <td><a href=<?php echo $pathImage; ?>><img src=<?php echo $pathImage; ?> width="100px" height="100px" alt="No image" class="img-category"></a></td>
+                        <td>
+                            <a href=<?php echo URL . "admin/update-category.php?id=" . $id; ?> class="btn-primary">Cập nhật</a>
+                            <a href=<?php echo URL . "admin/delete-item.php?id=" . $id . "&type=2"; ?> class="btn-danger">Xoá</a>
+                            <div class="clear-fix"></div>
+                        </td>
+                    </tr>
+                <?php
+                } else {
+                ?>
+                    <tr class="white text-center">
+                        <td><?php echo $i; ?></td>
+                        <td><?php echo $NameCategory; ?></td>
+                        <td><a href=<?php echo $pathImage; ?>><img src=<?php echo $pathImage; ?> width="100px" height="100px" alt="No image" class="img-category"></a></td>
+                        <td>
+                            <a href=<?php echo URL . "admin/update-category.php?id=" . $id; ?> class="btn-primary">Cập nhật</a>
+                            <a href=<?php echo URL . "admin/delete-item.php?id=" . $id . "&type=2"; ?> class="btn-danger">Xoá</a>
+                            <div class="clear-fix"></div>
+                        </td>
+                    </tr>
+            <?php
+                }
+            }
+            ?>
         </table>
 
     </div>
