402e9c2f7bda39d71d04e6bca105f37c31e1c20b
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-11-01 22:32:40 +0700

    fix layout manager-products

--- Admin/manager-products.php
+++ Admin/manager-products.php
@@ -37,46 +37,54 @@
             $listProducts = executeSQLResult($conn, $sql);
             for ($i = 1; $i <= count($listProducts); $i++) {
                 $id = $listProducts[$i - 1]['MSHH'];
-                $nameProduct = $listProducts[$i - 1]['TenHH'];
-                $description = $listProducts[$i - 1]['QuyCach'];
-                $price = $listProducts[$i - 1]['Gia'];
-                $quality = $listProducts[$i - 1]['SoLuongHang'];
-                $category = $listProducts[$i - 1]['TenLoaiHang'];
-                $pathImage = URL . 'images/products/' . $listProducts[$i - 1]['TenHinh'];
-                if ($i % 2 == 0) {
+
+                //check id same previous id
+                if ($id !== $listProducts[$i - 2]['MSHH']) {
+                    $nameProduct = $listProducts[$i - 1]['TenHH'];
+                    $description = $listProducts[$i - 1]['QuyCach'];
+                    $price = $listProducts[$i - 1]['Gia'];
+                    $quality = $listProducts[$i - 1]['SoLuongHang'];
+                    $category = $listProducts[$i - 1]['TenLoaiHang'];
+                    $pathImage = URL . 'images/products/' . $listProducts[$i - 1]['TenHinh'];
+                    if ($i % 2 == 0) {
             ?>
-                    <tr class="text-center">
-                        <td><?php echo $i; ?></td>
-                        <td class="name-product"><?php echo $nameProduct; ?></td>
-                        <td class="description"><div class="limit-height"><?php echo $description; ?></div></td>
-                        <td>VNĐ: <?php echo $price; ?></td>
-                        <td><?php echo $quality; ?></td>
-                        <td><?php echo $category; ?></td>
-                        <td><a href=<?php echo $pathImage; ?>><img src=<?php echo $pathImage; ?> width="100px" height="100px" alt="No image" class="img-category"></a></td>
-                        <td>
-                            <a href=<?php echo URL . "admin/update-product.php?id=" . $id; ?> class="btn-primary">Cập nhật</a>
-                            <a href=<?php echo URL . "admin/delete-item.php?id=" . $id . "&type=3"; ?> class="btn-danger">Xoá</a>
-                            <div class="clear-fix"></div>
-                        </td>
-                    </tr>
-                <?php
-                } else {
-                ?>
-                    <tr class="white text-center">
-                        <td><?php echo $i; ?></td>
-                        <td class="name-product"><?php echo $nameProduct; ?></td>
-                        <td class="description"><div class="limit-height"><?php echo $description; ?></div></td>
-                        <td>VNĐ: <?php echo $price; ?></td>
-                        <td><?php echo $quality; ?></td>
-                        <td><?php echo $category; ?></td>
-                        <td><a href=<?php echo $pathImage; ?>><img src=<?php echo $pathImage; ?> width="100px" height="100px" alt="No image" class="img-category"></a></td>
-                        <td>
-                            <a href=<?php echo URL . "admin/update-product.php?id=" . $id; ?> class="btn-primary">Cập nhật</a>
-                            <a href=<?php echo URL . "admin/delete-item.php?id=" . $id . "&type=3"; ?> class="btn-danger">Xoá</a>
-                            <div class="clear-fix"></div>
-                        </td>
-                    </tr>
+                        <tr class="text-center">
+                            <td><?php echo $i; ?></td>
+                            <td class="name-product"><?php echo $nameProduct; ?></td>
+                            <td class="description">
+                                <div class="limit-height"><?php echo $description; ?></div>
+                            </td>
+                            <td>VNĐ: <?php echo $price; ?></td>
+                            <td><?php echo $quality; ?></td>
+                            <td><?php echo $category; ?></td>
+                            <td><a href=<?php echo $pathImage; ?>><img src=<?php echo $pathImage; ?> width="100px" height="100px" alt="No image" class="img-category"></a></td>
+                            <td>
+                                <a href=<?php echo URL . "admin/update-product.php?id=" . $id; ?> class="btn-primary">Cập nhật</a>
+                                <a href=<?php echo URL . "admin/delete-item.php?id=" . $id . "&type=3"; ?> class="btn-danger">Xoá</a>
+                                <div class="clear-fix"></div>
+                            </td>
+                        </tr>
+                    <?php
+                    } else {
+                    ?>
+                        <tr class="white text-center">
+                            <td><?php echo $i; ?></td>
+                            <td class="name-product"><?php echo $nameProduct; ?></td>
+                            <td class="description">
+                                <div class="limit-height"><?php echo $description; ?></div>
+                            </td>
+                            <td>VNĐ: <?php echo $price; ?></td>
+                            <td><?php echo $quality; ?></td>
+                            <td><?php echo $category; ?></td>
+                            <td><a href=<?php echo $pathImage; ?>><img src=<?php echo $pathImage; ?> width="100px" height="100px" alt="No image" class="img-category"></a></td>
+                            <td>
+                                <a href=<?php echo URL . "admin/update-product.php?id=" . $id; ?> class="btn-primary">Cập nhật</a>
+                                <a href=<?php echo URL . "admin/delete-item.php?id=" . $id . "&type=3"; ?> class="btn-danger">Xoá</a>
+                                <div class="clear-fix"></div>
+                            </td>
+                        </tr>
             <?php
+                    }
                 }
             }
             ?>
