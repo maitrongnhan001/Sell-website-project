bc25dd1c6325ed23b0a070b146ae6ff4e7ec7732
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-11-02 20:27:37 +0700

    update api load products

--- Customer/API/handle-api-product.php
+++ Customer/API/handle-api-product.php
@@ -2,24 +2,32 @@
 include('../../Config/connect.php');
 //request
 if (isset($_GET['position']) && isset($_GET['limit'])) {
+    //get product
     $position = $_GET['position'];
     $limit = $_GET['limit'];
     $conn = connectToDatabase();
-    $sql = "SELECT HangHoa.MSHH, TenHH, QuyCach, Gia, TenLoaiHang, TenHinh 
-                FROM HangHoa, LoaiHangHoa, HinhHangHoa 
+    $sql = "SELECT HangHoa.MSHH, TenHH, QuyCach, Gia, TenLoaiHang 
+                FROM HangHoa, LoaiHangHoa 
                 WHERE HangHoa.MaLoaiHang = LoaiHangHoa.MaLoaiHang
-                AND HangHoa.MSHH = HinhHangHoa.MSHH LIMIT $limit OFFSET $position";
+                LIMIT $limit OFFSET $position";
     $result = executeSQLResult($conn, $sql);
+
     $list_product = array();
+
     for ($i = 0; $i < count($result); $i++) {
+        //get image
+        $id = $result[$i]['MSHH'];
+        $sql = "SELECT * FROM HinhHangHoa WHERE MSHH=$id LIMIT 1";
+        $result_image = executeSQLResult($conn, $sql);
+
         //convert to json
         $product = array(
-            'id' => $result[$i]['MSHH'],
+            'id' => $id,
             'name_product' => $result[$i]['TenHH'],
             'price' => $result[$i]['Gia'],
             'description' => $result[$i]['QuyCach'],
             'category' => $result[$i]['TenLoaiHang'],
-            'image_name' => $result[$i]['TenHinh']
+            'image_name' => $result_image[0]['TenHinh']
         );
 
         $list_product[$i] = $product;
